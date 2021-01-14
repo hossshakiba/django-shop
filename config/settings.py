0c69be02d824199b6f6552132ebf8de057ce23e9
Hoss <hossshakiba@oulook.com>
2021-01-14 22:58:46 +0330

    Implemented postgres as database

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD')
     }
 }
 
