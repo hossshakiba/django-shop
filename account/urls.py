56d758d066eef1528d66cf7ff7cca8bbc48585a3
Hoss <hossshakiba@oulook.com>
2021-01-17 18:34:50 +0330

    Fix some errorsand made some changes

--- account/urls.py
+++ account/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     path('logout', logout_view, name='logout'),
     path('password_reset/', auth_views.PasswordResetView.as_view(success_url = reverse_lazy('account:password_reset_done')), name ='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name ='password_reset_done'),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name ='password_reset_confirm'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(success_url = reverse_lazy('account:password_reset_complete')), name ='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name ='password_reset_complete')
 ]
--- config/settings.py
+++ config/settings.py
@@ -98,18 +98,12 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    
+    {  'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 6,
+        }
+    }
 ]
 
 
