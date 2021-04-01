40d670c4a9061751fb446f587dfddb15bd816229
Hoss <hossshakiba@outlook.com>
2021-04-01 20:44:10 +0430

    Create initial abstract product model

--- django_shop/products/models.py
+++ django_shop/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+class Product(models.Model):
+    """
+    Abstract product model contains common fields among all products
+    """
+    title = models.CharField('عنوان محصول', max_length=50)
+    description = models.TextField('توضیحات')
+    cover = models.ImageField('کاور محصول', upload_to=None)
+    price = models.PositiveIntegerField('قیمت')
+    discount_percent = models.PositiveSmallIntegerField('درصد تخفیف', blank=True, default=0)
+    available_count = models.PositiveSmallIntegerField('موجودی انبار')
+    is_active = models.BooleanField('فعال/غیرفعال', default=False)
+
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
 
